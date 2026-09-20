79a6920ca48922a557ac4dadbe55feee12521b87
N0zoM1z0 <1705004398@qq.com>
2026-09-20 04:50:28 +0000

    gpt-web: add TH04 nullfunc candidate

---
+++ src/main/core/nullfunc_far.cpp
@@ -0,0 +1,6 @@
+#pragma option -zCCIRCLE_TEXT -zPmain_01
+#pragma option -k-
+
+extern "C" void pascal far nullfunc_far(void)
+{
+}
---
+++ src/main/core/nullfunc_near.cpp
@@ -0,0 +1,6 @@
+#pragma option -zCCIRCLE_TEXT -zPmain_01
+#pragma option -k-
+
+extern "C" void pascal near nullfunc_near(void)
+{
+}
